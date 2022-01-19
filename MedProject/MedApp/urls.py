c767b2c494b40d60fe1b4a868b6f87934badafe0
Nikita <n.necheparenko@g.nsu.ru>
2022-01-19 18:25:10 +0300

    Детальный просмотр пациента по его id, с возможность редактирования (put)

--- MedProject/MedApp/urls.py
+++ MedProject/MedApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PatientList
+from .views import PatientList, PatientView
 
 
 urlpatterns = [
-    path('patients/', PatientList.as_view(), name='patients_list')
+    path('patients/', PatientList.as_view(), name='patients_list'),
+    path('patients/<int:pk>', PatientView.as_view(), name='patient_detail')
 ]
--- MedProject/MedApp/views.py
+++ MedProject/MedApp/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Patient
@@ -6,6 +7,8 @@ from .serializers import PatientSerializer
 
 
 class PatientList(APIView):
+    """Просмотр всех клиентов на одной странице (пагинация не предусмотрена)"""
+
     def get(self, request):
         patients = Patient.objects.all()
         serializer = PatientSerializer(patients, many=True)
@@ -17,3 +20,26 @@ class PatientList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class PatientView(APIView):
+    """Детальный просмотр клиента по его pk (primary key = id)"""
+
+    def get_object(self, pk):
+        try:
+            return Patient.objects.get(pk=pk)
+        except Patient.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        patient = self.get_object(pk)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        patient = Patient.get_object(pk)
+        serializer = PatientSerializer(patient, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
